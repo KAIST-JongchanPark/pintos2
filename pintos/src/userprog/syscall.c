1b8ca795492c18ffb1adbca93447bdc50d7c819d
Donghyun Kim <dong32@gmail.com>
2019-03-27 20:32:59 +0900

    memory access without exit status implemented

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -3,8 +3,28 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "userprog/pagedir.c"
 
 static void syscall_handler (struct intr_frame *);
+
+void* valid_pointer(void* ptr)
+{
+	if(!is_user_vaddr(ptr))
+	{
+		//exit with status -1
+		return 0;
+	} 
+	if(!pagedir_get_page(thread_current()->pagedir, ptr))
+	{
+		//exit with status -1
+		return 0;
+	}
+	return ptr;
+}
+
+void
+exit_with_status(void)
 
 void
 syscall_init (void) 
