3b534c4bb936402bdc9e1fd08c551d38adef52dd
biryuyeon <whdals1060@gmail.com>
2019-03-28 22:38:20 +0900

    fix typo

--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -2,7 +2,7 @@
 #define USERPROG_SYSCALL_H
 
 void* is_valid_ptr(void*);
-void exit_with_status (void);
+void exit_with_status (int);
 int write (int, const void *, unsigned);
 void syscall_init (void);
 
