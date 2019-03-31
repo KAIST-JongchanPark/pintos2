81ae72c0831aeeadb76edc4d8c86775ad84d7fc1
Donghyun Kim <dong32@gmail.com>
2019-03-31 20:08:09 +0900

    fix bad read write

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -109,6 +109,8 @@ int filesize (int fd)
 
 int read (int fd, void *buffer, unsigned size)
 {
+  if(fd<0||fd>=128)
+    return -1;
   if(fd==0)
   {
     int i;
@@ -133,6 +135,8 @@ int read (int fd, void *buffer, unsigned size)
 
 int write (int fd, const void *buffer, unsigned size) 
 {
+  if(fd<0||fd>=128)
+    return -1;
   if (fd == 1) 
   {
     putbuf(buffer, size);
