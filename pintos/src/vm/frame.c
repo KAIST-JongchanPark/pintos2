47de962fad852539f37c0237d9219ea979b0adf8
Donghyun Kim <dong32@gmail.com>
2019-04-28 20:31:06 +0900

    fix typo

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -13,7 +13,7 @@ void
 frame_init (void)
 {
 	list_init(&frame_table);
-	printf("frame_init\n")
+	printf("frame_init\n");
 }
 
 
@@ -30,7 +30,7 @@ allocate_frame (void *addr, struct sup_page_table_entry* spte)
 	fte -> owner = thread_current();
 	fte -> spte = spte;
 	list_push_front(&frame_table, &(fte->elem));
-	printf("alloc_frame\n")
+	printf("alloc_frame\n");
 	return 1;
 
 }
