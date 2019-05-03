5272848e68e1be893ec433a0a467694ef4f9d87b
biryuyeon <whdals1060@gmail.com>
2019-05-03 19:28:29 +0900

    test

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -105,6 +105,7 @@ bool allocate_using_spt(void* addr, struct sup_page_table_entry *spte)
 	  allocate_frame((void *)kpage);
 	  file_seek (file, ofs);
 	  /* Load this page. */
+	  ASSERT(file!=NULL);
 	  int read_bytes = file_read (file, kpage, page_read_bytes);
 	  ASSERT(read_bytes>0);
 	  if (read_bytes != (int) page_read_bytes)
