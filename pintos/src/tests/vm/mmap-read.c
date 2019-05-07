1685bdb1081457822c8cb250c8f8ca4f85dc524c
biryuyeon <whdals1060@gmail.com>
2019-05-08 01:22:22 +0900

    test

--- pintos/src/tests/vm/mmap-read.c
+++ pintos/src/tests/vm/mmap-read.c
@@ -17,6 +17,7 @@ test_main (void)
   CHECK ((handle = open ("sample.txt")) > 1, "open \"sample.txt\"");
   msg("handle: %d\n", handle);
   CHECK ((map = mmap (handle, actual)) != MAP_FAILED, "mmap \"sample.txt\"");
+  msg("actual: %x\n", actual);
   msg("mmap read test00\n");
   /* Check that data is correct. */
   int ret_memcmp = memcmp (actual, sample, strlen (sample));
