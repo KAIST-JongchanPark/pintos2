e073c697cb8e0ffb7f3dcdf8c44a3217b7f1d750
biryuyeon <whdals1060@gmail.com>
2019-05-08 01:04:51 +0900

    delete panic

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -257,7 +257,7 @@ void close(int fd)
 
 mapid_t mmap (int fd, void *addr)
 {
-  PANIC("mmap syscall test");
+  //PANIC("mmap syscall test");
   //invalid fd
   if (fd<0||fd>=128||fd==0||fd==1)
     return -1;
