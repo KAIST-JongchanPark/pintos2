deac54c613c3331ff6f73d17f50ddd6dfe9b52d8
biryuyeon <whdals1060@gmail.com>
2019-05-10 03:03:03 +0900

    test

--- pintos/src/vm/swap.c
+++ pintos/src/vm/swap.c
@@ -130,6 +130,7 @@ swap_out (void) // when palloc is null, page full.
 		return true;
 	}
 	size_t place = bitmap_scan(swap_table, 0, 8, false);
+	printf("addr4: %x\n", upage);
 	if(place==BITMAP_ERROR)
 	{
 		PANIC("swap slots are fully used.");
@@ -140,7 +141,7 @@ swap_out (void) // when palloc is null, page full.
 		write_to_disk(kpage, place+i);
 	}
 	bitmap_set_multiple(swap_table, place, 8, true);
-	printf("addr4: %x\n", upage);
+	//안됨
 	free_frame(upage);
 	pagedir_set_dirty(pd, upage, false);
 	pagedir_set_dirty(pd, kpage, false);
