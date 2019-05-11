6f24f0361b279ad843697f4138df5ad6a3ff6cc0
biryuyeon <whdals1060@gmail.com>
2019-05-11 21:49:05 +0900

    test

--- pintos/src/vm/swap.h
+++ pintos/src/vm/swap.h
@@ -8,6 +8,6 @@ void swap_init (void);
 bool swap_in (void *addr);
 bool swap_out (void);
 void read_from_disk (uint8_t *frame, int index);
-void write_to_disk (uint8_t *frame, int index);
+void write_to_disk (uint8_t *frame, size_t place, int index);
 
 #endif /* vm/swap.h */
