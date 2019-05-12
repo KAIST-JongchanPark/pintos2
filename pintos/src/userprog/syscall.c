8cba77ab64438e32bdd04fe5de3c2798bec57db9
biryuyeon <whdals1060@gmail.com>
2019-05-12 23:12:05 +0900

    test

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -176,12 +176,12 @@ int read (int fd, void *buffer, unsigned size)
 	//printf("test1");
     return -1;
   //lock_acquire(&syscall_lock);
-  if(!lookup_spt(buffer)&&pg_ofs(buffer)!=0)
-  {
-	  printf("error addr: %x\n", buffer);
-	  printf("stack addr: %x\n", thread_current()->stack);
-	  exit_with_status(-1);
-  }
+  //if(!lookup_spt(buffer)&&pg_ofs(buffer)!=0)
+  //{
+	  //printf("error addr: %x\n", buffer);
+	  //printf("stack addr: %x\n", thread_current()->stack);
+	  //exit_with_status(-1);
+  //}
   if(fd==0)
   {
     int i;
