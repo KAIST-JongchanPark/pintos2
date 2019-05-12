48de61f801bc21532cb7634561469dad190b7798
biryuyeon <whdals1060@gmail.com>
2019-05-12 22:00:22 +0900

    test

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -200,6 +200,7 @@ page_fault (struct intr_frame *f)
 	  //valid but not present in spt?? heap data, init to zero
 	  if(!lookup_spt(fault_addr))
 	  {
+		  printf("not spt addr: %x\n", fault_addr);
 		  //printf("not lookup spt: %x\n", fault_addr);
 		  //allocate_and_init_to_zero(fault_addr);
 		  if(fault_addr>HEURISTIC&&(fault_addr>=stack_pointer||fault_addr==stack_pointer-4||fault_addr==stack_pointer-32))
