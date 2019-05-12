282b43c94a0a002d7ba13cd9e4048955b6d0b070
biryuyeon <whdals1060@gmail.com>
2019-05-12 22:10:06 +0900

    test

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -53,12 +53,12 @@ void* is_valid_ptr(void* ptr)
 		exit_with_status(-1);
 		return 0;
 	} 
-	if(!lookup_spt(ptr))
-	{
+	//if(!lookup_spt(ptr))
+	//{
 		//exit with status -1
-		exit_with_status(-2);
-		return 0;
-	}
+	//	exit_with_status(-1);
+	//	return 0;
+	//}
 	return ptr;
 }
 
