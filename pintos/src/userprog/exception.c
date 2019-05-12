c649f20b4b01ef4cc715c4da616291d262f0ac72
biryuyeon <whdals1060@gmail.com>
2019-05-12 23:22:39 +0900

    test

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -195,7 +195,7 @@ page_fault (struct intr_frame *f)
   else*/
   if(fault_addr>=0x08048000)
   {
-	  printf("addr: %x\n", fault_addr);
+	  //printf("addr: %x\n", fault_addr);
 	  //printf("addr: %x", fault_addr);
 	  //valid but not present in spt?? heap data, init to zero
 	  if(!lookup_spt(fault_addr))
