43743bcd96dbf59ce200842237cc6bb94d9bf729
biryuyeon <whdals1060@gmail.com>
2019-05-13 04:22:11 +0900

    test

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -59,12 +59,13 @@ void* is_valid_ptr(void* ptr)
 		//printf("not spt addr: %x\n", ptr);
 		printf("not spt addr: %x\n", ptr);
 		int *temp = ptr+PGSIZE;
+		printf("what is temp: %d\n", *temp);
 		while((thread_current()->fd[*temp]==NULL )&&is_user_vaddr(temp))
 		{
 			temp+=PGSIZE;
 			printf("not spt calc addr: %x\n", temp);
 		}
-		printf("what is temp: %d\n", *temp);
+		//printf("what is temp: %d\n", *temp);
 		if(is_user_vaddr(temp))
 		{
 			exit_with_status(-1);
