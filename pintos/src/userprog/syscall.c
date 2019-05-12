ccb53bc3cb4e7c6895609e6047473fe342244bfd
biryuyeon <whdals1060@gmail.com>
2019-05-13 04:21:03 +0900

    test

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -64,7 +64,7 @@ void* is_valid_ptr(void* ptr)
 			temp+=PGSIZE;
 			printf("not spt calc addr: %x\n", temp);
 		}
-		
+		printf("what is temp: %d\n", *temp);
 		if(is_user_vaddr(temp))
 		{
 			exit_with_status(-1);
