9c670b8fd5de5eef3bdb999434cee856ab71e632
Donghyun Kim <dong32@gmail.com>
2019-05-13 21:54:31 +0900

    test

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -151,7 +151,7 @@ bool allocate_using_spt(void* addr, struct sup_page_table_entry *spte)
 		printf("hi ");
 		pagedir_set_dirty(thread_current()->pagedir, upage, false);
 	}
-	if(pagedir_is_dirty(thread_current()->pagedir, upage));
+	if(pagedir_is_dirty(thread_current()->pagedir, upage))
        {
         printf("is dirty\n");
        }
