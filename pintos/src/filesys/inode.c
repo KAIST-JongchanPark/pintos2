a0ce7d4f5beaea5f850f3ac53c781edc1af5b334
biryuyeon <whdals1060@gmail.com>
2019-05-23 02:14:30 +0900

    intr test

--- pintos/src/filesys/inode.c
+++ pintos/src/filesys/inode.c
@@ -138,7 +138,7 @@ inode_open (disk_sector_t sector)
   inode->open_cnt = 1;
   inode->deny_write_cnt = 0;
   inode->removed = false;
-  cache_read (filesys_disk, inode->sector, &inode->data);
+  disk_read (filesys_disk, inode->sector, &inode->data);
   return inode;
 }
 
