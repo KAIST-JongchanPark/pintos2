5f8200a618c4401a77cf659ec0ea35a19d3dfa03
biryuyeon <whdals1060@gmail.com>
2019-05-27 02:21:15 +0900

    fix typo

--- pintos/src/filesys/directory.c
+++ pintos/src/filesys/directory.c
@@ -25,7 +25,7 @@ struct dir_entry
 /* Creates a directory with space for ENTRY_CNT entries in the
    given SECTOR.  Returns true if successful, false on failure. */
 bool
-dir_create (disk_sector_t sector, size_t entry_cnt, const char *dir_name) 
+dir_create (disk_sector_t sector, size_t entry_cnt) 
 {
   bool result = inode_create (sector, entry_cnt * sizeof (struct dir_entry), true);
   /*
