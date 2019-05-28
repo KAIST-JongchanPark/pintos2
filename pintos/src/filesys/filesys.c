a48f3f8063d8283f21d6e326f284b15756fa2a6f
biryuyeon <whdals1060@gmail.com>
2019-05-28 20:51:31 +0900

    test

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -78,7 +78,7 @@ filesys_create (const char *name, off_t initial_size, bool is_dir)
   bool success = (dir != NULL
                   && free_map_allocate (1, &inode_sector)
                   && inode_create (inode_sector, initial_size, is_dir)
-                  && dir_add (dir, file_name, inode_sector));
+                  && dir_add (dir, name, inode_sector));
   if (!success && inode_sector != 0) 
     free_map_release (inode_sector, 1);
   dir_close (dir);
@@ -179,7 +179,7 @@ filesys_open (const char *name)
 
   if (dir != NULL)
   {
-    dir_lookup (dir, file_name, &inode);
+    dir_lookup (dir, name, &inode);
   }
   printf("reached\n");
   dir_close (dir);
