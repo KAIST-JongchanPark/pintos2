4712e5ff4da4d878dc49892ef639d686f5800176
Donghyun Kim <dong32@gmail.com>
2019-05-29 16:46:46 +0900

    typo

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -81,7 +81,7 @@ filesys_create (const char *name, off_t initial_size, bool is_dir)
   //printf("name: %s\n", name);
   char *file_name = get_name(name);
   printf("file_name: %s\n", file_name);
-  bool success = (dir != NULL
+  bool success = dir != NULL
                   && free_map_allocate (1, &inode_sector);
   printf("success value %d\n", success);
                   success = success&& inode_create (inode_sector, initial_size, is_dir);
